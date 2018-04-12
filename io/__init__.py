8752678d8c94fa1af4f360be95ad77ad90adb3f8
Loris Ercole <loris.ercole@gmail.com>
2018-04-12 17:09:19 +0200

    Fixed io/__init__.py

--- io/__init__.py
+++ io/__init__.py
@@ -5,5 +5,5 @@ __all__ = [ 'read_tablefile', 'read_lammps_dump', 'read_lammps_log', 'read_lammp
 from . import *
 from .read_tablefile import TableFile
 from .read_lammps_dump import LAMMPS_Dump
-from .read_lammps_log import Read_LAMMPSlogfile
+from .read_lammps_log import LAMMPSLogFile
 
