2cff04f97f39bd6da2da495bed434e76263579db
Loris Ercole <loris.ercole@gmail.com>
2019-11-15 11:27:06 +0100

    new setup: install GUI only if Python 3

--- setup.py
+++ setup.py
@@ -1,27 +1,50 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
 from setuptools import setup, find_packages
+from os import path
+import sys
 import json
 
-with open('README_GUI.md', 'r') as fh:
-    long_description = fh.read()
-    with open('thermocepstrum/README_GUI.md', 'w') as fc:
-        fc.write(long_description)
+GUI_dependencies = [
+        "future-fstrings",
+        "markdown2",
+        "pillow>=5.4.0",
+        "tk-html-widgets",
+        "uncertainties"
+]
 
-with open('README.md', 'r') as fh:
-    long_description = fh.read()
-    with open('thermocepstrum/README.md', 'w') as fc:
-        fc.write(long_description)
+if sys.version_info[0] == 2:
+    INSTALL_GUI = False
+    sys.stderr.write("************* GUI does not support Python 2: it will not be installed. That's a pity! *************")
+elif sys.version_info[0] == 3:
+    INSTALL_GUI = True
+
 
 if __name__ == '__main__':
-    with open('thermocepstrum/setup.json', 'r') as info:
-        kwargs = json.load(info)
+    THIS_FOLDER = path.split(path.abspath(__file__))[0]
+
+    with open(path.join(THIS_FOLDER, 'README.md'), 'r') as fh:
+        long_description = fh.read()
+
+    with open(path.join(THIS_FOLDER, 'setup.json'), 'r') as info:
+        SETUP_JSON = json.load(info)
+
+    if INSTALL_GUI:
+        exclude_packages = []
+        SETUP_JSON['install_requires'] += GUI_dependencies
+        SETUP_JSON['entry_points']['console_scripts'] += ["thermocepstrum-gui = thermocepstrum_gui.main:run"]
+        with open(path.join(THIS_FOLDER, 'thermocepstrum/metadata.json'), 'w') as fh:
+           json.dump(SETUP_JSON, fh)
+    else:
+        exclude_packages = ['thermocepstrum_gui*']
 
     setup(include_package_data=True,
-          packages=find_packages(),   # exclude=['docs','tests*'] ...
-          setup_requires=['reentry'],
-          reentry_register=True,
+          packages=find_packages(exclude=exclude_packages),
           long_description=long_description,
           long_description_content_type='text/markdown',
-          **kwargs)
+#          setup_requires=['reentry'],
+          **SETUP_JSON)
 
 # https://packaging.python.org/guides/distributing-packages-using-setuptools/#setup-args
 # https://setuptools.readthedocs.io/en/latest/setuptools.html#including-data-files
