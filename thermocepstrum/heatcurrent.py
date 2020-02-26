Implementation of the mel-filter of the log of PSD now the implementation of the mean values of the filtered logspectrum is correct and follow the computations on the notes. Still the computation of the variance is not complete

thermocepstrum/heatcurrent.py
@@ -37,7 +37,7 @@ class HeatCurrent(MDSample):
      - freq_units    frequency units   [THz or red] (optional)
     """
 
-    def __init__(self, j, units, DT_FS, TEMPERATURE, VOLUME, PSD_FILTER_W=None, freq_units='THz', do_mel=False, mel_scale=1e12, mel_nrecursion=1, mel_nfilt=None,mel_log_flag=False):
+    def __init__(self, j, units, DT_FS, TEMPERATURE, VOLUME, PSD_FILTER_W=None, freq_units='THz', do_mel=False, mel_scale=1e12, mel_nrecursion=1, mel_nfilt=None,mel_log_flag=True):
 
         # check if we have a multicomponent fluid
         j = np.array(j, dtype=float)
@@ -86,13 +86,14 @@ class HeatCurrent(MDSample):
                 else:
                     raise ValueError('Freq units not valid.')
             
-            self.initialize_cepstral_parameters()
 
             if self.do_mel:
                 if(self.mel_log_flag):
                     self.compute_mel_filter_log()
                 else:
                     self.compute_mel_filter()
+
+            self.initialize_cepstral_parameters()
         else:
             log.write_log('Warning: trajectory not initialized. You should manually initialize what you need.')
 
@@ -167,6 +168,7 @@ class HeatCurrent(MDSample):
         Defines the parameters of the theoretical distribution of the cepstrum.
         """
         if not self.many_currents:
+            self.ndf_chi = self.N_COMPONENTS
             self.ck_THEORY_var, self.psd_THEORY_mean = \
                 md.cepstral.multicomp_cepstral_parameters(self.Nfreqs, self.N_COMPONENTS)
         else:
@@ -174,6 +176,9 @@ class HeatCurrent(MDSample):
                 raise RuntimeError('self.ndf_chi cannot be None.')
             self.ck_THEORY_var, self.psd_THEORY_mean = \
                 md.cepstral.multicomp_cepstral_parameters(self.Nfreqs, self.ndf_chi)
+        if self.do_mel :
+            self.mel_ck_THEORY_var, self.mel_psd_THEORY_mean = \
+                md.cepstral.mel_multicomp_cepstral_parameters(N_COMPONENTS=self.ndf_chi,bins=self.mel_bins)
         return
 
     def cepstral_analysis(self, aic_type='aic', Kmin_corrfactor=1.0, K_PSD=None):
@@ -215,8 +220,8 @@ class HeatCurrent(MDSample):
             appa_Kmin  +/-  kappa_Kmin_std   [W/(m*K)]
         """
 
-        self.mel_dct = md.CosFilter(self.mel_logpsd, ck_theory_var=self.ck_THEORY_var, \
-            psd_theory_mean=self.psd_THEORY_mean, aic_type=aic_type, Kmin_corrfactor=Kmin_corrfactor)
+        self.mel_dct = md.CosFilter(self.mel_logpsd, ck_theory_var=self.mel_ck_THEORY_var, \
+            psd_theory_mean=self.mel_psd_THEORY_mean, aic_type=aic_type, Kmin_corrfactor=Kmin_corrfactor)
         self.mel_dct.scan_filter_tau(K_PSD=K_PSD)
         self.mel_kappa_Kmin = self.mel_dct.tau_Kmin * self.kappa_scale * 0.5
         self.mel_kappa_Kmin_std = self.mel_dct.tau_std_Kmin * self.kappa_scale * 0.5
@@ -496,7 +501,7 @@ def resample_current(x, TSKIP=None, fstar_THz=None, FILTER_W=None, plot=True, PS
         '                             =  {:12.3f} fs\n'.format(TSKIP * x.DT_FS) +\
         ' Original  n. of frequencies =  {:12d}\n'.format(x.Nfreqs) +\
         ' Resampled n. of frequencies =  {:12d}\n'.format(xf.Nfreqs)
-    if x.fpsd is not None:
+    if x.fpsd is not None and xf.fpsd is not None:
         xf.resample_log += \
             ' PSD      @cutoff  (pre-filter) = {:12.5f}\n'.format(x.fpsd[fstar_idx]) +\
             '                  (post-filter) = {:12.5f}\n'.format(xf.fpsd[-1]) +\

thermocepstrum/md/mdsample.py
@@ -470,7 +470,8 @@ class MDSample(object):
         out[0] = arr[0]
         out[-1] = arr[int(bins[-1])]
         print('out={}'.format(out) )
-        return out, melpoints
+        #N_bins = bins - np.roll(bins[-1])
+        return out, melpoints,bins
 
     def mel_interpolate(self, melpoints, y, nfft, nrec=1):
         """Interpolate the Mel-filtered spectrum.
@@ -503,7 +504,7 @@ class MDSample(object):
         if self.mel_nfilt is None:
            self.mel_nfilt = self.Nfreqs//10
 
-        self.mel_filtered, self.mel_points = self.mel_filter( nfilt=self.mel_nfilt, samplerate=int(1e15/self.DT_FS),
+        self.mel_filtered, self.mel_points ,self.mel_bins = self.mel_filter( nfilt=self.mel_nfilt, samplerate=int(1e15/self.DT_FS),
                                                        lowfreq=0, highfreq=self.Nyquist_f_THz*1e12, axis=0, nrec=self.mel_nrecursion, triang=triang)
 
         self.mel_filtered_freqs, self.mel_filtered_psd = self.mel_interpolate(self.mel_points, self.mel_filtered, self.Nfreqs, nrec=self.mel_nrecursion)
@@ -527,17 +528,19 @@ class MDSample(object):
 
         arr = self.logpsd
 
-        self.mel_filtered, self.mel_points = self.mel_filter(arr=arr, nfilt=self.mel_nfilt,
+        self.mel_filtered, self.mel_points, self.mel_bins = self.mel_filter(arr=arr, nfilt=self.mel_nfilt,
                                                              samplerate=int(1e15 / self.DT_FS),
                                                              lowfreq=0, highfreq=self.Nyquist_f_THz * 1e12, axis=0,
                                                              nrec=self.mel_nrecursion, triang=triang)
 
+
         ### TODO magari cambiare i nomi delle variabili
-        self.mel_filtered_freqs, self.mel_logpsd = self.mel_interpolate(self.mel_points, self.mel_filtered,
-                                                                              self.Nfreqs, nrec=self.mel_nrecursion)
+        #self.mel_filtered_freqs, self.mel_filtered_logpsd = self.mel_interpolate(self.mel_points, self.mel_filtered,
+        #                                                                    self.Nfreqs, nrec=self.mel_nrecursion)
+
         self.mel_filtered_psd = np.copy(self.psd)
-        self.mel_filtered_freqs_THz = self.mel_filtered_freqs * 1e-12
-        #self.mel_logpsd = np.log(self.mel_filtered_psd)
+        self.mel_filtered_freqs_THz = self.mel2hz_rec(self.mel_points,self.mel_nrecursion)*1e-12 #self.mel_filtered_freqs * 1e-12
+        self.mel_logpsd = self.mel_filtered
         self.mel_psd_min = np.min(self.mel_filtered_psd)
         self.mel_psd_power = np.trapz(self.mel_filtered_psd)  # one-side PSD power
         return