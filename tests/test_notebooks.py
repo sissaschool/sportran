6a7920693790e983bc75a9c650586e7673c1fb51
Riccardo Bertossa <riccardo.bertossa@gmail.com>
2021-11-04 19:28:03 +0100

    ooops... forgot to put an absolute path

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -1,15 +1,18 @@
 # -*- coding: utf-8 -*-
 import pytest
 from testbook import testbook
+import os
+
+test_folder_path = os.path.dirname(os.path.abspath(__file__))
 
 
-@testbook('../examples/example_cepstrum_doublecomp_NaCl.ipynb', execute=True)
+@testbook(test_folder_path + '/../examples/example_cepstrum_doublecomp_NaCl.ipynb', execute=True)
 def test_doublecomp_NaCl(tb, file_regression):
     res = tb.cell_output_text('results_cell')
     file_regression.check(res, basename='final_result1')
 
 
-@testbook('../examples/example_cepstrum_singlecomp_silica.ipynb', execute=True)
+@testbook(test_folder_path + '/../examples/example_cepstrum_singlecomp_silica.ipynb', execute=True)
 def test_singlecomp_silica(tb, file_regression):
     res = tb.cell_output_text('results_cell')
     file_regression.check(res, basename='final_result2')
