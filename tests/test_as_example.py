d40c0c115ace869f0a1649b26c2997ecae7b7d71
Riccardo Bertossa <riccardo.bertossa@gmail.com>
2022-01-28 16:44:04 +0100

    added little test for fixed number ok K

--- tests/test_as_example.py
+++ tests/test_as_example.py
@@ -30,6 +30,19 @@ def test_example_SiO2(data_SiO2, check_reg):
     check_reg(jf)
 
 
+def test_example_SiO2_fixed_K(data_SiO2, check_reg):
+    import sportran as st
+
+    jfile, DT_FS, TEMPERATURE, VOLUME = data_SiO2
+    j = st.HeatCurrent(jfile.data['flux1'], DT_FS=DT_FS, UNITS='metal', TEMPERATURE=TEMPERATURE, VOLUME=VOLUME)
+    print(j.Nyquist_f_THz)
+    FSTAR_THZ = 28.0
+    jf = j.resample(fstar_THz=FSTAR_THZ, plot=False, freq_units='thz')
+    jf.cepstral_analysis(manual_cutoffK=42)
+    print(jf.cepstral_log)
+    check_reg(jf)
+
+
 if __name__ == '__main__':
     test_example_NaCl()
     test_example_SiO2()
