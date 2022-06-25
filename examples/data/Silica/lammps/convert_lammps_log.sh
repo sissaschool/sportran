be124735d893899dd018f5ebbbf9bbc3da5d94b3
Loris Ercole <loris.ercole@gmail.com>
2022-06-25 17:38:26 +0200

    Add LAMMPS Silica input/output files
    Fixes #18
    Contributes to #23
    The LAMMPS input files used to generate the silica example data have
    been added to the `examples/data/Silica/lammps` folder.
    In the same folder, the output of the calculation is reported, along
    with a script to convert it to `table` and `dict` formats.
    The data had to be regenerated, therefore all the data files in the
    example folder have been updated.

---
+++ examples/data/Silica/lammps/convert_lammps_log.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Convert the silica.out LAMMPS log file into:
+#  - Silica.dat :  a TableFile (a table-style text file with data sorted in columns)
+#  - Silica.npy :  a NumPy binary pickle file
+
+LAMMPS_FILE='silica.out'
+SPORTRAN_DIR='../../../../sportran'
+
+# convert to table format
+${SPORTRAN_DIR}/i_o/extract_lammps_thermo.sh -f ${LAMMPS_FILE} -d 'DUMP_RUN' > Silica.dat
+
+# convert to numpy binary
+python ${SPORTRAN_DIR}/i_o/read_lammps_log.py ${LAMMPS_FILE} Silica.npy -s silica_216_1000K.init -d 'DUMP_RUN' 
