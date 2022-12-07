bc559d0f251b04a664debb798ec1faaac45ac8b9
Riccardo Bertossa <33728857+rikigigi@users.noreply.github.com>
2022-12-07 13:18:36 +0100

    fixes #66 (#67)
    * fixes #66 - there was a bug in `sportran-analysis` when the moving average window was not specified.
    * added test for cli when the user don't ask for the filtering of the data with the moving average approach
    * version updated to 1.0rc3
    
    Co-authored-by: Loris Ercole <loris.ercole@gmail.com>

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -35,6 +35,27 @@ def test_cli_NaCl(tmpdir, run_cli, data_NaCl_path, num_regression, file_regressi
     num_regression.check(readed)
 
 
+def test_cli_NaCl_no_w(tmpdir, run_cli, data_NaCl_path, num_regression, file_regression):
+    fileout_pref = tmpdir.join('output_no_w')
+    output = run_cli(data_NaCl_path, '-k', 'flux', '-j', 'vcm[1]', '-t', '5.0', '--VOLUME', '65013.301261',
+                     '--param-from-input-file-column', 'Temp', 'TEMPERATURE', '--FSTAR', '14.0', '-r',
+                     '--test-suite-run', '-o', fileout_pref)
+    file_list = [
+        'output_no_w.cepstral.dat', 'output_no_w.cepstrumfiltered_psd.dat', 'output_no_w.cospectrum.dat',
+        'output_no_w.psd.dat', 'output_no_w.resampled_psd.dat'
+    ]
+    with open(str(tmpdir.join('output_no_w.log'))) as l:
+        file_regression.check(l.read(), basename='output_no_w.log')
+    file_regression.check(output.stdout.str(), basename='stdout_no_w')
+    file_regression.check(output.stderr.str(), basename='stderr_no_w')
+    readed = {}
+    for f in file_list:
+        file_out = tmpdir.join(f)
+        readed[f] = np.loadtxt(file_out).flatten()
+
+    num_regression.check(readed)
+
+
 def test_cli_bin_output_NaCl(tmpdir, run_cli, data_NaCl_path, num_regression, file_regression):
     fileout_pref = tmpdir.join('output')
     output = run_cli(data_NaCl_path, '-k', 'flux', '-j', 'vcm[1]', '-t', '5.0', '--VOLUME', '65013.301261',
